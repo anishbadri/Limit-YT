8d13e98ee0e43268f82490ef8be4c8832dcaecd4
AnishBadri <anishbadri@gmail.com>
2019-10-23 10:00:15 +0530

    extract video id

--- background.js
+++ background.js
@@ -5,11 +5,36 @@ chrome.browserAction.setBadgeText({text: '0'})
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     // console.log(changeInfo.url);
     const youTubeRegex = new RegExp("/*youtube.com/watch*")
-    if(youTubeRegex.exec(changeInfo.url))
+    if(isUrlActiveYTVideo(changeInfo.url))
     {
+        videoId = getVideoIdFromUrl(changeInfo.url);
+
+        if(videoId){
+            //save video ID
+                
+        } 
+        console.log(videoId);
         videoCount++;
         console.log("Url changed to ", changeInfo.url);
         chrome.browserAction.setBadgeText({text: videoCount.toString()})
-
     }
 })
+
+
+function isUrlActiveYTVideo(url){
+    //watch regex
+    const youTubeWatchRegex = new RegExp("/*youtube.com/watch*");
+    if(youTubeWatchRegex.exec(url)){
+        return true;
+    }
+    return false;
+}
+
+function getVideoIdFromUrl(url){
+    //works only for a striaght forward Url 
+    const regex = /https\:\/\/www\.youtube\.com\/watch\?v=([\w-]{11})/;
+    return url.match(regex) ? url.match(regex)[1]: null;
+}
+
+
+
